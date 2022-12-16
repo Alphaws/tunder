6bf45c83df26a309ab511672c04489ed25be08b6
Alphaws <alphaws@gmail.com>
2022-12-16 03:56:03 +0100

    tailwind

---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+    <img src="{{ asset('img/logo1.png') }}" alt="Tündérmosoly" class="w-32">
+</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <div class="hidden fixed top-0 right-0 px-6 py-4 bg-gray-100 sm:block">
                 @auth
                     <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                 @else
@@ -15,9 +15,12 @@
         @endif
 
         <div class="w-full mx-auto">
-            <div class="flex justify-center items-center p-16 bg-gray-800">
-                <img src="{{ asset('img/logo1.png') }}" alt="Tündérmosoly" class="w-16">
-                <span class="px-4 text-gray-200 text-xl">Tündérmosoly Alapítvány</span>
+            <div class="grid grid-cols-3 gap-4 p-16 m-2 bg-gray-800 text-gray-200">
+                <div class="flex justify-center items-center">
+                    <img src="{{ asset('img/logo1.png') }}" alt="Tündérmosoly" class="w-16">
+                    <span class="px-4 text-gray-200 text-2xl">Tündérmosoly Alapítvány</span>
+                </div>
+                <div class="col-span-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
             </div>
 
             <div class="flex justify-center items-center p-16 bg-green-800">
